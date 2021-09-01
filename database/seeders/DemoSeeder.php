2d97fd42eec98a17db0884821590616c561cf8be
ronx38 <xjinxprohard@gmail.com>
2021-09-01 10:22:22 +0700

    seed notif

--- database/seeders/DemoSeeder.php
+++ database/seeders/DemoSeeder.php
@@ -103,7 +103,8 @@ class DemoSeeder extends Seeder
                 'image' => 'Images.Png',
                 'content' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
                 'preview' => '',
-                'publish_status' => 2,
+                'publish_status' => rand(0,1),
+                'publish_date' =>  Carbon::now(),
                 'created_by' => 'demo' . $i . '@mailinator.com',
                 'updated_by' => 'demo' . $i . '@mailinator.com',
                 'created_at' => Carbon::now(),
@@ -121,18 +122,18 @@ class DemoSeeder extends Seeder
         }
         //Doctor
         for ($i = 1; $i <= 100; $i++) {
-                DB::table('doctor')->insertGetId([
-                    'user_id' => rand(1, 10),
-                    'doctor_category_id' => rand(1, 10),
-                    'price' => '99.000',
-                    'formal_edu' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-                    'nonformal_edu' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-                    'created_at' => Carbon::now(),
-                    'updated_at' => Carbon::now(),
-                ]);
-            }
-          //Lab
-          for ($i = 1; $i <= 100; $i++) {
+            DB::table('doctor')->insertGetId([
+                'user_id' => rand(1, 10),
+                'doctor_category_id' => rand(1, 10),
+                'price' => '99.000',
+                'formal_edu' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                'nonformal_edu' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+        }
+        //Lab
+        for ($i = 1; $i <= 100; $i++) {
             DB::table('lab')->insertGetId([
                 'parent_id' => 0,
                 'name' => 'DEMO ' . $i,
@@ -142,12 +143,27 @@ class DemoSeeder extends Seeder
                 'desc_lab' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
                 'desc_benefit' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
                 'desc_preparation' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-                'recommended_for' => 'Pria','Wanita','Lansia','Anak-anak',
+                'recommended_for' => json_encode(['Pria','Wanita','Lansia','Anak-anak']),
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now(),
             ]);
-        }  
+        }
 
-        
+        //Notif
+        for ($i = 1; $i <= 100; $i++) {
+            DB::table('notifications')->insertGetId([
+                'user_id' => rand(1, 10),
+                'title' => 'notif'.$i,
+                'message' => 'qui officia deserunt mollit anim id est laborum.',
+                'content' => 'sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                'target' => '',
+                'is_read' => rand(0, 1),
+                'type' => 'notifications',
+                'date' => Carbon::now(),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+        }
+
     }
 }
