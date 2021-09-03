07e498031effe5bb06a7d80174fa9595ee741835
MisleadX <vincentj7319@gmail.com>
2021-09-03 14:22:02 +0700

    Update Klinik, dan Path Models

---
+++ app/Http/Controllers/Admin/KlinikController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Codes\Logic\_CrudController;
+use App\Codes\Models\V1\Klinik;
+use Illuminate\Http\Request;
+
+class KlinikController extends _CrudController
+{
+    public function __construct(Request $request)
+    {
+        $passingData = [
+            'id' => [
+                'create' => 0,
+                'edit' => 0,
+                'show' => 0
+            ],
+            'name' => [
+                'validate' => [
+                    'create' => 'required',
+                    'edit' => 'required'
+                ]
+            ],
+            'status' => [
+                'validate' => [
+                    'create' => 'required',
+                    'edit' => 'required'
+                ],
+                'type' => 'select',
+            ],
+        ];
+
+        parent::__construct(
+            $request, 'general.klinik', 'klinik', 'V1\Klinik', 'klinik',
+            $passingData
+        );
+
+    }
+}
