52397dd9c7f748aa05dcaaa300fe3db9cd654752
Hendra PC <hendratjua@gmail.com>
2021-09-15 10:36:20 +0700

    update token in error

--- app/Codes/Models/V1/Payment.php
+++ app/Codes/Models/V1/Payment.php
@@ -16,4 +16,24 @@ class Payment extends Model
         'type',
         'status'
     ];
+
+    protected $appends = [
+        'icon_full',
+        'price_nice',
+    ];
+
+    public function getImageFullAttribute()
+    {
+        return asset('assets/cms/images/no-img.png');
+        if (strlen($this->icon) > 0) {
+            return env('OSS_URL').'/'.$this->icon;
+        }
+        return asset('assets/cms/images/no-img.png');
+    }
+
+    public function getPriceNiceAttribute()
+    {
+        return intval($this->price) > 0 ? number_format($this->price, 0, '.', '.') : 0;
+    }
+
 }
