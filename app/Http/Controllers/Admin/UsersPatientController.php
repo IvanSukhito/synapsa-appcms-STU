530ecca3715db0225e8061a2f5dbd9c868e0d0b8
IvanSukhito <sukhitoo.ivan@gmail.com>
2021-09-20 12:09:38 +0700

    update cms article

---
+++ app/Http/Controllers/Admin/UsersPatientController.php
@@ -0,0 +1,366 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Codes\Logic\_CrudController;
+use App\Codes\Models\V1\City;
+use App\Codes\Models\V1\District;
+use App\Codes\Models\V1\Doctor;
+use App\Codes\Models\V1\SubDistrict;
+use App\Codes\Models\V1\Users;
+use App\Codes\Models\V1\Klinik;
+use Yajra\DataTables\DataTables;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Http\Request;
+
+class UsersPatientController extends _CrudController
+{
+    public function __construct(Request $request)
+    {
+        $passingData = [
+            'id' => [
+                'create' => 0,
+                'edit' => 0,
+                'show' => 0
+            ],
+            'klinik_id' => [
+                'validate' => [
+                    'create' => 'required',
+                    'edit' => 'required'
+                ],
+                'lang' => 'general.klinik',
+                'type' => 'select2',
+            ],
+            'city_id' => [
+                'validate' => [
+                    'create' => 'required',
+                    'edit' => 'required'
+                ],
+                'lang' => 'general.city',
+                'type' => 'select2',
+                'list' => 0,
+            ],
+            'district_id' => [
+                'validate' => [
+                    'create' => 'required',
+                    'edit' => 'required'
+                ],
+                'lang' => 'general.district',
+                'type' => 'select2',
+                'list' => 0,
+            ],
+            'sub_district_id' => [
+                'validate' => [
+                    'create' => 'required',
+                    'edit' => 'required'
+                ],
+                'lang' => 'general.sub_district',
+                'type' => 'select2',
+                'list' => 0,
+            ],
+            'fullname' => [
+                'validate' => [
+                    'create' => 'required',
+                    'edit' => 'required'
+                ]
+            ],
+            'address' => [
+                'validate' => [
+                    'create' => 'required',
+                    'edit' => 'required'
+                ],
+                'type' => 'textarea',
+                'list' => 0,
+            ],
+            'address_detail' => [
+                'validate' => [
+                    'create' => 'required',
+                    'edit' => 'required'
+                ],
+                'type' => 'textarea',
+                'list' => 0,
+            ],
+            'zip_code' => [
+                'validate' => [
+                    'create' => 'required',
+                    'edit' => 'required'
+                ],
+                'list' => 0,
+            ],
+            'dob' => [
+                'validate' => [
+                    'create' => 'required',
+                    'edit' => 'required'
+                ],
+                'type' => 'datepicker',
+                'list' => 0,
+            ],
+            'gender' => [
+                'validate' => [
+                    'create' => 'required',
+                    'edit' => 'required'
+                ],
+                'type' => 'select',
+            ],
+            'nik' => [
+                'validate' => [
+                    'create' => 'required',
+                    'edit' => 'required'
+                ],
+                'list' => 0,
+            ],
+            'upload_ktp' => [
+                'validate' => [
+                    'create' => 'required',
+                    'edit' => 'required'
+                ],
+                'path' => 'synapapps/users',
+                'type' => 'image',
+                'list' => 0,
+            ],
+            'phone' => [
+                'validate' => [
+                    'create' => 'required',
+                    'edit' => 'required'
+                ],
+            ],
+            'email' => [
+                'validate' => [
+                    'create' => 'required',
+                    'edit' => 'required'
+                ],
+                'type' => 'email',
+            ],
+            'status' => [
+                'validate' => [
+                    'create' => 'required',
+                    'edit' => 'required'
+                ],
+                'type' => 'select',
+            ],
+            'action' => [
+                'create' => 0,
+                'edit' => 0,
+                'show' => 0,
+                'lang' => 'Aksi',
+            ]
+        ];
+
+        parent::__construct(
+            $request, 'general.users_patient', 'users-patient', 'V1\Users', 'users-patient',
+            $passingData
+        );
+
+        $getKlinik = Klinik::where('status', 80)->pluck('name', 'id')->toArray();
+        if($getKlinik) {
+            foreach($getKlinik as $key => $value) {
+                $listKlinik[$key] = $value;
+            }
+        }
+
+        $getCity = City::pluck('name', 'id')->toArray();
+        $listCity = [0 => 'Kosong'];
+        if($getCity) {
+            foreach($getCity as $key => $value) {
+                $listCity[$key] = $value;
+            }
+        }
+
+        $getDistrict = District::pluck('name', 'id')->toArray();
+        $listDistrict = [0 => 'Kosong'];
+        if($getDistrict) {
+            foreach($getDistrict as $key => $value) {
+                $listDistrict[$key] = $value;
+            }
+        }
+
+        $getSubDistrict = SubDistrict::pluck('name', 'id')->toArray();
+        if($getSubDistrict) {
+            foreach($getSubDistrict as $key => $value) {
+                $listSubDistrict[$key] = $value;
+            }
+        }
+
+        $this->data['listSet']['klinik_id'] = $listKlinik;
+        $this->data['listSet']['city_id'] = $listCity;
+        $this->data['listSet']['gender'] = get_list_gender();
+        $this->data['listSet']['status'] = get_list_active_inactive();
+        $this->data['listSet']['district_id'] = $listDistrict;
+        $this->data['listSet']['sub_district_id'] = $listSubDistrict;
+    }
+
+    public function store(){
+        
+        $this->callPermission();
+
+        $viewType = 'create';
+
+        $getListCollectData = collectPassingData($this->passingData, $viewType);
+        $validate = $this->setValidateData($getListCollectData, $viewType);
+        if (count($validate) > 0)
+        {
+            $data = $this->validate($this->request, $validate);
+        }
+        else {
+            $data = [];
+            foreach ($getListCollectData as $key => $val) {
+                $data[$key] = $this->request->get($key);
+            }
+        }
+
+        $dokument = $data['upload_ktp'];
+        if ($dokument) {
+            if ($dokument->getError() != 1) {
+
+                $getFileName = $dokument->getClientOriginalName();
+                $ext = explode('.', $getFileName);
+                $ext = end($ext);
+                $destinationPath = 'synapapps/users';
+                if (in_array(strtolower($ext), ['jpg', 'jpeg', 'png', 'svg', 'gif'])) {
+
+                    $dokumentImage = Storage::putFile($destinationPath, $dokument);
+                }
+
+            }
+        }
+
+    
+        $data = $this->getCollectedData($getListCollectData, $viewType, $data);
+
+        $data['upload_ktp'] = $dokumentImage;
+        $data['password'] = bcrypt('123');
+        $data['patient'] = 1;
+        $getData = $this->crud->store($data);
+
+        $id = $getData->id;
+
+        if($this->request->ajax()){
+            return response()->json(['result' => 1, 'message' => __('general.success_add_', ['field' => $this->data['thisLabel']])]);
+        }
+        else {
+            session()->flash('message', __('general.success_add_', ['field' => $this->data['thisLabel']]));
+            session()->flash('message_alert', 2);
+            return redirect()->route($this->rootRoute.'.' . $this->route . '.index');
+        }   
+    }
+    
+    public function update($id){
+        
+        $this->callPermission();
+
+        $viewType = 'edit';
+
+        $getData = $this->crud->show($id);
+        if (!$getData) {
+            return redirect()->route($this->rootRoute.'.' . $this->route . '.index');
+        }
+
+        $getListCollectData = collectPassingData($this->passingData, $viewType);
+        $validate = $this->setValidateData($getListCollectData, $viewType);
+        if (count($validate) > 0)
+        {
+            $data = $this->validate($this->request, $validate);
+        }
+        else {
+            $data = [];
+            foreach ($getListCollectData as $key => $val) {
+                $data[$key] = $this->request->get($key);
+            }
+        }
+
+        $dokument = $data['upload_ktp'];
+        if ($dokument) {
+            if ($dokument->getError() != 1) {
+
+                $getFileName = $dokument->getClientOriginalName();
+                $ext = explode('.', $getFileName);
+                $ext = end($ext);
+                $destinationPath = 'synapapps/users';
+                if (in_array(strtolower($ext), ['jpg', 'jpeg', 'png', 'svg', 'gif'])) {
+
+                    $dokumentImage = Storage::putFile($destinationPath, $dokument);
+                }
+
+            }
+        }
+
+    
+        $data = $this->getCollectedData($getListCollectData, $viewType, $data);
+
+        $data['upload_ktp'] = $dokumentImage;
+
+        $getData = $this->crud->update($data, $id);
+
+        $id = $getData->id;
+
+        if($this->request->ajax()){
+            return response()->json(['result' => 1, 'message' => __('general.success_add_', ['field' => $this->data['thisLabel']])]);
+        }
+        else {
+            session()->flash('message', __('general.success_add_', ['field' => $this->data['thisLabel']]));
+            session()->flash('message_alert', 2);
+            return redirect()->route($this->rootRoute.'.' . $this->route . '.index');
+        }   
+    }
+    public function dataTable()
+    {
+        $this->callPermission();
+
+        //$userId = session()->get('admin_id');
+
+        $dataTables = new DataTables();
+
+        $builder = $this->model::query()->selectRaw('users.id, users.fullname, users.gender, users.email, users.phone, klinik.name as klinik_id, users.status')
+            ->where('users.patient', '=', 1)
+            ->leftJoin('klinik','klinik.id','=','users.klinik_id')
+            ->where('klinik.status',80);
+
+
+        $dataTables = $dataTables->eloquent($builder)
+            ->addColumn('action', function ($query) {
+                return view($this->listView['dataTable'], [
+                    'query' => $query,
+                    'thisRoute' => $this->route,
+                    'permission' => $this->permission,
+                    'masterId' => $this->masterId
+                ]);
+            });
+
+        $listRaw = [];
+        $listRaw[] = 'action';
+        foreach (collectPassingData($this->passingData) as $fieldName => $list) {
+            if (in_array($list['type'], ['select', 'select2'])) {
+                $dataTables = $dataTables->editColumn($fieldName, function ($query) use ($fieldName) {
+                    $getList = isset($this->data['listSet'][$fieldName]) ? $this->data['listSet'][$fieldName] : [];
+                    return isset($getList[$query->$fieldName]) ? $getList[$query->$fieldName] : $query->$fieldName;
+                });
+            } else if (in_array($list['type'], ['money'])) {
+                $dataTables = $dataTables->editColumn($fieldName, function ($query) use ($fieldName, $list, $listRaw) {
+                    return number_format($query->$fieldName, 0);
+                });
+            } else if (in_array($list['type'], ['image'])) {
+                $listRaw[] = $fieldName;
+                $dataTables = $dataTables->editColumn($fieldName, function ($query) use ($fieldName, $list, $listRaw) {
+                    return '<img src="' . asset($list['path'] . $query->$fieldName) . '" class="img-responsive max-image-preview"/>';
+                });
+            } else if (in_array($list['type'], ['image_preview'])) {
+                $listRaw[] = $fieldName;
+                $dataTables = $dataTables->editColumn($fieldName, function ($query) use ($fieldName, $list, $listRaw) {
+                    return '<img src="' . $query->$fieldName . '" class="img-responsive max-image-preview"/>';
+                });
+            } else if (in_array($list['type'], ['code'])) {
+                $listRaw[] = $fieldName;
+                $dataTables = $dataTables->editColumn($fieldName, function ($query) use ($fieldName, $list, $listRaw) {
+                    return '<pre>' . json_encode(json_decode($query->$fieldName, true), JSON_PRETTY_PRINT) . '"</pre>';
+                });
+            } else if (in_array($list['type'], ['texteditor'])) {
+                $listRaw[] = $fieldName;
+            }
+        }
+
+        return $dataTables
+            ->rawColumns($listRaw)
+            ->make(true);
+    }
+
+}
