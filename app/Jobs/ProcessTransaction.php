0501db4d8016fc81afb3a2a3b9f0c84294fd96bd
Hendra PC <hendratjua@gmail.com>
2021-09-29 15:49:09 +0700

    update transaction product

--- app/Jobs/ProcessTransaction.php
+++ app/Jobs/ProcessTransaction.php
@@ -246,9 +246,11 @@ class ProcessTransaction implements ShouldQueue
             'phone' => $getUser->phone ?? ''
         ];
 
-        foreach (['address_name', 'address', 'city_id', 'city_name', 'district_id', 'district_name',
-                     'sub_district_id', 'sub_district_name', 'zip_code'] as $key) {
-            $extraInfo[$key] = isset($getUsersAddress->$key) ? $getUsersAddress->$key : '';
+        if ($getUsersAddress) {
+            foreach (['address_name', 'address', 'city_id', 'city_name', 'district_id', 'district_name',
+                         'sub_district_id', 'sub_district_name', 'zip_code'] as $key) {
+                $extraInfo[$key] = $getUsersAddress->$key;
+            }
         }
 
         DB::beginTransaction();
@@ -371,18 +373,15 @@ class ProcessTransaction implements ShouldQueue
 
         $extraInfo = [
             'service_id' => $getLabSchedule->service_id,
-            'address_name' => $getUsersAddress->address_name ?? '',
-            'address' => $getUsersAddress->address ?? '',
-            'city_id' => $getUsersAddress->city_id ?? '',
-            'district_id' => $getUsersAddress->district_id ?? '',
-            'sub_district_id' => $getUsersAddress->sub_district_id ?? '',
-            'zip_code' => $getUsersAddress->zip_code ?? '',
             'phone' => $getUser->phone ?? ''
         ];
 
-        foreach (['address_name', 'address', 'city_id', 'city_name', 'district_id', 'district_name',
-        'sub_district_id', 'sub_district_name', 'zip_code'] as $key) {
-        $extraInfo[$key] = $getUsersAddress->$key;}
+        if ($getUsersAddress) {
+            foreach (['address_name', 'address', 'city_id', 'city_name', 'district_id', 'district_name',
+                         'sub_district_id', 'sub_district_name', 'zip_code'] as $key) {
+                $extraInfo[$key] = $getUsersAddress->$key;
+            }
+        }
 
         DB::beginTransaction();
 
