cf2b74e10ebef24c720d9e3f0075e6c1c85710f9
ronx38 <xjinxprohard@gmail.com>
2021-10-15 14:53:13 +0700

    Update shipping

---
+++ app/Http/Controllers/Admin/ShippingController.php
@@ -0,0 +1,204 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Codes\Logic\_CrudController;
+use App\Codes\Models\V1\DoctorService;
+use Illuminate\Http\Request;
+use App\Codes\Models\V1\Payment;
+use Illuminate\Support\Facades\Storage;
+
+class ShippingController extends _CrudController
+{
+    public function __construct(Request $request)
+    {
+        $passingData = [
+            'id' => [
+                'create' => 0,
+                'edit' => 0,
+                'show' => 0
+            ],
+            'name' => [
+                'validate' => [
+                    'create' => 'required',
+                    'edit' => 'required'
+                ],
+           ],
+
+            'icon_full' => [
+                'validate' => [
+                    'create' => 'required',
+                ],
+                'lang' => 'icon_img',
+                'type' => 'image',
+            ],
+            'orders' => [
+                'validate' => [
+                    'create' => 'required',
+                    'edit' => 'required'
+                ],
+                'type' => 'number',
+            ],
+            'settings' => [
+                'validate' => [
+                    'create' => 'required',
+                    'edit' => 'required'
+                ],
+                'type' => 'textarea',
+                'list' => 0,
+            ],
+            'status' => [
+                'validate' => [
+                    'create' => 'required',
+                    'edit' => 'required'
+                ],
+                'type' => 'select',
+           ],
+            'action' => [
+                'create' => 0,
+                'edit' => 0,
+                'show' => 0,
+                'custom' => ',orderable:false'
+            ]
+        ];
+
+        parent::__construct(
+            $request, 'general.shipping', 'shipping', 'V1\Shipping', 'shipping',
+            $passingData
+        );
+
+        $this->data['listSet']['status'] = get_list_active_inactive();
+        $this->data['listSet']['type_payment'] = get_list_type_payment();
+        $this->data['listSet']['service'] = get_list_service_payment();
+
+    }
+    public function store()
+    {
+        $this->callPermission();
+
+        $viewType = 'create';
+
+        $getListCollectData = collectPassingData($this->passingData, $viewType);
+
+        unset($getListCollectData['icon_full']);
+
+        $validate = $this->setValidateData($getListCollectData, $viewType);
+        if (count($validate) > 0)
+        {
+            $data = $this->validate($this->request, $validate);
+        }
+        else {
+            $data = [];
+            foreach ($getListCollectData as $key => $val) {
+                $data[$key] = $this->request->get($key);
+            }
+        }
+
+        $dokument = $this->request->file('icon_full');
+        if ($dokument) {
+            if ($dokument->getError() != 1) {
+
+                $getFileName = $dokument->getClientOriginalName();
+                $ext = explode('.', $getFileName);
+                $ext = end($ext);
+                $destinationPath = 'synapsaapps/shipping';
+                if (in_array(strtolower($ext), ['jpg', 'jpeg', 'png', 'svg', 'gif'])) {
+
+                    $dokumentImage = Storage::putFile($destinationPath, $dokument);
+                }
+
+            }
+        }
+
+        $settings = $data['settings'];
+
+
+        $data = $this->getCollectedData($getListCollectData, $viewType, $data);
+
+        $data['icon'] = $dokumentImage;
+        $data['settings'] = json_encode($settings);
+
+        $getData = $this->crud->store($data);
+
+
+        $id = $getData->id;
+
+        if($this->request->ajax()){
+            return response()->json(['result' => 1, 'message' => __('general.success_add_', ['field' => $this->data['thisLabel']])]);
+        }
+        else {
+            session()->flash('message', __('general.success_add_', ['field' => $this->data['thisLabel']]));
+            session()->flash('message_alert', 2);
+            return redirect()->route($this->rootRoute.'.' . $this->route . '.index');
+        }
+    }
+
+    public function update($id){
+        $this->callPermission();
+
+        $viewType = 'edit';
+
+        $getData = $this->crud->show($id);
+        if (!$getData) {
+            return redirect()->route($this->rootRoute.'.' . $this->route . '.index');
+        }
+
+        $getListCollectData = collectPassingData($this->passingData, $viewType);
+
+        unset($getListCollectData['icon']);
+
+        $validate = $this->setValidateData($getListCollectData, $viewType, $id);
+        if (count($validate) > 0)
+        {
+            $data = $this->validate($this->request, $validate);
+        }
+        else {
+            $data = [];
+            foreach ($getListCollectData as $key => $val) {
+                $data[$key] = $this->request->get($key);
+            }
+        }
+
+        $dokument = $this->request->file('icon');
+        if ($dokument) {
+            if ($dokument->getError() != 1) {
+
+                $getFileName = $dokument->getClientOriginalName();
+                $ext = explode('.', $getFileName);
+                $ext = end($ext);
+                $destinationPath = 'synapsaapps/shipping';
+                if (in_array(strtolower($ext), ['jpg', 'jpeg', 'png', 'svg', 'gif'])) {
+
+                    $dokumentImage = Storage::putFile($destinationPath, $dokument);
+                }
+
+            }
+        }else{
+
+            $dokumentImage = $getData->icon_img;
+
+        }
+
+        $settings = $data['settings'];
+
+        $data = $this->getCollectedData($getListCollectData, $viewType, $data, $getData);
+
+
+        $data['icon'] = $dokumentImage;
+        $data['settings'] = json_encode($settings);
+
+        $getData = $this->crud->update($data, $id);
+
+        $id = $getData->id;
+
+        if($this->request->ajax()){
+            return response()->json(['result' => 1, 'message' => __('general.success_add_', ['field' => $this->data['thisLabel']])]);
+        }
+        else {
+            session()->flash('message', __('general.success_add_', ['field' => $this->data['thisLabel']]));
+            session()->flash('message_alert', 2);
+            return redirect()->route($this->rootRoute.'.' . $this->route . '.index');
+        }
+    }
+
+}
