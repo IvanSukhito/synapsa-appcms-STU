7e672b5c3cfd9de794211509ea7fe191c41f88e8
IvanSukhito <sukhitoo.ivan@gmail.com>
2021-10-15 14:28:58 +0700

    update db address

--- app/Http/Controllers/API/V1/DoctorAppointmentController.php
+++ app/Http/Controllers/API/V1/DoctorAppointmentController.php
@@ -307,10 +307,46 @@ class DoctorAppointmentController extends Controller
 
     }
 
-    public function cancelMeeting($id)
-    {
+//    public function cancelMeeting($id)
+//    {
+//        $user = $this->request->attributes->get('_user');
+//        $getDoctor = Doctor::where('user_id', $user->id)->first();
+//        if (!$getDoctor) {
+//            return response()->json([
+//                'success' => 1,
+//                'message' => ['Hanya menu untuk dokter'],
+//                'token' => $this->request->attributes->get('_refresh_token'),
+//            ], 422);
+//        }
+//
+//        $data = AppointmentDoctor::whereIn('status', [1,2])->where('doctor_id', $getDoctor->id)->where('id', $id)->first();
+//        if (!$data) {
+//            return response()->json([
+//                'success' => 0,
+//                'message' => ['Janji Temu Dokter Tidak Ditemukan'],
+//                'token' => $this->request->attributes->get('_refresh_token'),
+//            ], 404);
+//        }
+//
+//        $data->status = 99;
+//        $data->save();
+//
+//        return response()->json([
+//            'success' => 1,
+//            'message' => ['Sukses Di Batalkan'],
+//            'token' => $this->request->attributes->get('_refresh_token'),
+//        ]);
+//
+//    }
+
+    public function reschedule($id){
+
         $user = $this->request->attributes->get('_user');
+
+        $message = strip_tags($this->request->get('message'));
+
         $getDoctor = Doctor::where('user_id', $user->id)->first();
+
         if (!$getDoctor) {
             return response()->json([
                 'success' => 1,
@@ -319,25 +355,28 @@ class DoctorAppointmentController extends Controller
             ], 422);
         }
 
-        $data = AppointmentDoctor::whereIn('status', [1,2])->where('doctor_id', $getDoctor->id)->where('id', $id)->first();
-        if (!$data) {
-            return response()->json([
-                'success' => 0,
-                'message' => ['Janji Temu Dokter Tidak Ditemukan'],
-                'token' => $this->request->attributes->get('_refresh_token'),
-            ], 404);
-        }
+       $data = AppointmentDoctor::whereIn('status', [1,2])->where('doctor_id', $getDoctor->id)->where('id', $id)->first();
+       if (!$data) {
+           return response()->json([
+               'success' => 0,
+               'message' => ['Janji Temu Dokter Tidak Ditemukan'],
+               'token' => $this->request->attributes->get('_refresh_token'),
+           ], 404);
+       }
 
-        $data->status = 99;
+        $data->status = 2;
+        $data->message = $message;
         $data->save();
 
-        return response()->json([
-            'success' => 1,
-            'message' => ['Sukses Di Batalkan'],
-            'token' => $this->request->attributes->get('_refresh_token'),
-        ]);
+       return response()->json([
+           'success' => 1,
+           'message' => ['Sukses Reschedule'],
+           'token' => $this->request->attributes->get('_refresh_token'),
+       ]);
 
     }
+
+
 
     public function doctorMedicine($id)
     {
