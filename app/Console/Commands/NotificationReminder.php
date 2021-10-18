b0eefc3eff1c2cdd5561c2b6270b714083f35af9
Hendra PC <hendratjua@gmail.com>
2021-10-18 13:12:06 +0700

    update meeting every minute

---
+++ app/Console/Commands/NotificationReminder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Codes\Logic\PushNotification;
+use Illuminate\Console\Command;
+
+class NotificationReminder extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'notification:push';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Notification Reminder FCM & Database channel';
+
+    /**
+     * Create a new command instance.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle(): void
+    {
+        (new PushNotification())->checkMeeting(5);
+    }
+
+}
