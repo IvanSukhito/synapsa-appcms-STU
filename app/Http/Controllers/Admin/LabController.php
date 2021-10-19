5725c053bcc0cfc1872756905809d1045ce1ac48
MisleadX <vincentj7319@gmail.com>
2021-10-19 15:29:42 +0700

    Lab

--- app/Http/Controllers/Admin/LabController.php
+++ app/Http/Controllers/Admin/LabController.php
@@ -70,7 +70,6 @@ class LabController extends _CrudController
                 ],
                 'type' => 'multiselect2',
                 'list' => 0,
-                'show'  => 0,
             ],
             'action' => [
                 'create' => 0,
@@ -118,7 +117,7 @@ class LabController extends _CrudController
             }
         }
 
-        $dokument = $this->request->file('image');
+        $dokument = $this->request->file('image_full');
         if ($dokument) {
             if ($dokument->getError() != 1) {
 
@@ -163,5 +162,115 @@ class LabController extends _CrudController
             session()->flash('message_alert', 2);
             return redirect()->route($this->rootRoute.'.' . $this->route . '.index');
         }
+    }
+
+    public function update($id){
+        $this->callPermission();
+
+        $viewType = 'edit';
+
+        $getListCollectData = collectPassingData($this->passingData, $viewType);
+
+        unset($getListCollectData['image_full']);
+
+        $validate = $this->setValidateData($getListCollectData, $viewType);
+        if (count($validate) > 0)
+        {
+            $data = $this->validate($this->request, $validate);
+        }
+        else {
+            $data = [];
+            foreach ($getListCollectData as $key => $val) {
+                $data[$key] = $this->request->get($key);
+            }
+        }
+
+        $dokument = $this->request->file('image_full');
+        $dokumentImage = null;
+        if ($dokument) {
+            if ($dokument->getError() != 1) {
+
+                $getFileName = $dokument->getClientOriginalName();
+                $ext = explode('.', $getFileName);
+                $ext = end($ext);
+                $destinationPath = 'synapsaapps/lab';
+                if (in_array(strtolower($ext), ['jpg', 'jpeg', 'png', 'svg', 'gif'])) {
+
+                    $dokumentImage = Storage::putFile($destinationPath, $dokument);
+                }
+
+            }
+        }
+
+
+        $statusPublish = $this->request->get('publish_status');
+        if($statusPublish == null){
+            $publish = 0;
+        }else{
+            $publish = 1;
+        }
+
+
+        $recommend = $data['recommended_for'];
+
+        $data = $this->getCollectedData($getListCollectData, $viewType, $data);
+
+        $data['image'] = $dokumentImage;
+        $data['recommended_for'] = json_encode($recommend);
+
+        $getData = $this->crud->update($data, $id);
+
+        $id = $getData->id;
+
+        if($this->request->ajax()){
+            return response()->json(['result' => 1, 'message' => __('general.success_add_', ['field' => $this->data['thisLabel']])]);
+        }
+        else {
+            session()->flash('message', __('general.success_add_', ['field' => $this->data['thisLabel']]));
+            session()->flash('message_alert', 2);
+            return redirect()->route($this->rootRoute.'.' . $this->route . '.show', $id);
+        }
+    }
+
+    public function show($id)
+    {
+        $this->callPermission();
+
+        $getData = $this->crud->show($id);
+        if (!$getData) {
+            return redirect()->route($this->rootRoute.'.' . $this->route . '.index');
+        }
+
+        $getData->recommended_for = json_decode($getData->recommended_for);
+
+        $data = $this->data;
+
+        $data['viewType'] = 'show';
+        $data['formsTitle'] = __('general.title_show', ['field' => $data['thisLabel']]);
+        $data['passing'] = collectPassingData($this->passingData, $data['viewType']);
+        $data['data'] = $getData;
+
+        return view($this->listView[$data['viewType']], $data);
+    }
+
+    public function edit($id)
+    {
+        $this->callPermission();
+
+        $getData = $this->crud->show($id);
+        if (!$getData) {
+            return redirect()->route($this->rootRoute.'.' . $this->route . '.index');
+        }
+
+        $getData->recommended_for = json_decode($getData->recommended_for);
+
+        $data = $this->data;
+
+        $data['viewType'] = 'edit';
+        $data['formsTitle'] = __('general.title_edit', ['field' => $data['thisLabel']]);
+        $data['passing'] = collectPassingData($this->passingData, $data['viewType']);
+        $data['data'] = $getData;
+
+        return view($this->listView[$data['viewType']], $data);
     }
 }
