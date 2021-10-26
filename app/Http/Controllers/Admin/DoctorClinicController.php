6e446e0a718e07dfaa7ff3c6a85d41a9f117b566
IvanSukhito <sukhitoo.ivan@gmail.com>
2021-10-26 13:12:27 +0700

    import schedule

--- app/Http/Controllers/Admin/DoctorClinicController.php
+++ app/Http/Controllers/Admin/DoctorClinicController.php
@@ -733,6 +733,11 @@ class DoctorClinicController extends _CrudController
 
         if($getFile) {
 
+            $destinationPath = 'synapsaapps/doctor-schedule/example_import';
+
+            $getUrl = Storage::put($destinationPath, $getFile);
+
+            die(env('OSS_URL') . '/' . $getUrl);
             try {
                 $getFileName = $getFile->getClientOriginalName();
                 $ext = explode('.', $getFileName);
