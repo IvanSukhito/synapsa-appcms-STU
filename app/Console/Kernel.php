4de5d48ba25bba4f4fe8b6a7d4abee56180c689f
Hendra PC <hendratjua@gmail.com>
2021-11-05 14:36:15 +0700

    update expired transaction

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     {
         $schedule->command('notification:push')->everyMinute();
         $schedule->command('auto:finishMeeting')->everyThirtyMinutes();
-        $schedule->command('auto:expiredTransaction')->everyTwoHours();
+        $schedule->command('auto:expiredTransaction')->hourly();
     }
 
     /**
