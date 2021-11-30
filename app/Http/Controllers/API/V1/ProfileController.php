be610cb3b5bedd7e6d6ba4b37c55182b6d53aa9b
IvanSukhito <sukhitoo.ivan@gmail.com>
2021-11-30 11:30:12 +0700

    update api

--- app/Http/Controllers/API/V1/ProfileController.php
+++ app/Http/Controllers/API/V1/ProfileController.php
@@ -69,9 +69,9 @@ class ProfileController extends Controller
         }
 
         $getUploadKtp = '';
-        if ($this->request->get('upload_ktp')) {
+        if ($this->request->file('upload_ktp')) {
              try {
-                 $image = base64_to_jpeg($this->request->get('upload_ktp'));
+                 $image = base64_to_jpeg($this->request->file('upload_ktp'));
                  $destinationPath = 'synapsaapps/users';
                  $set_file_name = md5('image'.strtotime('now').rand(0, 100)).'.jpg';
                  $getFile = Storage::put($destinationPath.'/'.$set_file_name, $image);
@@ -137,7 +137,7 @@ class ProfileController extends Controller
         }
 
         try {
-            $image = base64_to_jpeg($this->request->get('image'));
+            $image = base64_to_jpeg($this->request->file('image'));
             $destinationPath = 'synapsaapps/users';
             $set_file_name = md5('image'.strtotime('now').rand(0, 100)).'.jpg';
             $getFile = Storage::put($destinationPath.'/'.$set_file_name, $image);
@@ -406,7 +406,7 @@ class ProfileController extends Controller
         }
 
         try {
-            $image = base64_to_jpeg($this->request->get('image'));
+            $image = base64_to_jpeg($this->request->file('image'));
             $destinationPath = 'synapsaapps/chat/user_'.$user->id;
             $set_file_name = md5('image'.strtotime('now').rand(0, 100)).'.jpg';
             $getFile = Storage::put($destinationPath.'/'.$set_file_name, $image);
