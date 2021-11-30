5732928a054c84cd1785a450ee50ef233bbdf15c
IvanSukhito <sukhitoo.ivan@gmail.com>
2021-11-30 10:40:58 +0700

    update api

--- app/Http/Controllers/API/V1/GeneralController.php
+++ app/Http/Controllers/API/V1/GeneralController.php
@@ -101,7 +101,7 @@ class GeneralController extends Controller
         $getUploadImage = '';
         if ($this->request->get('image')) {
             try {
-                $image = base64_to_jpeg($this->request->get('upload_ktp'));
+                $image = base64_to_jpeg($this->request->get('image'));
                 $destinationPath = 'synapsaapps/users';
                 $set_file_name = md5('image' . strtotime('now') . rand(0, 100)) . '.jpg';
                 $getFile = Storage::put($destinationPath . '/' . $set_file_name, $image);
