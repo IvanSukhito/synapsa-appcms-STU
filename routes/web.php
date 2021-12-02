83d1cdb766fb705f2fa7c4051f0d976056a7bbd8
Hendra PC <hendratjua@gmail.com>
2021-12-02 14:08:35 +0700

    update apppointment

--- routes/web.php
+++ routes/web.php
@@ -149,7 +149,7 @@ Route::post('change-password', ['uses' => 'App\Http\Controllers\Website\V1\Gener
 Route::get('confirm-email', ['uses' => 'App\Http\Controllers\Website\V1\GeneralController@confirmEmail'])->name('web.confirmEmail');
 Route::get('confirm-phone', ['uses' => 'App\Http\Controllers\Website\V1\GeneralController@confirmPhone'])->name('web.confirmPhone');
 
-Route::get('/', ['uses' => 'App\Http\Controllers\Website\V1\GeneralController@xendit'])->name('web.xendit');
+//Route::get('/', ['uses' => 'App\Http\Controllers\Website\V1\GeneralController@xendit'])->name('web.xendit');
 
 //UsersPatient
 Route::get('/findCity', ['uses' => 'App\Http\Controllers\Admin\GeneralController@findCity'])->name('admin.findCity');
@@ -158,6 +158,6 @@ Route::get('/findSubDistrict', ['uses' => 'App\Http\Controllers\Admin\GeneralCon
 Route::get('/findProductSynapsa', ['uses' => 'App\Http\Controllers\Admin\GeneralController@findProductSynapsa'])->name('admin.findProductSynapsa');
 Route::get('/appointment-lab-schedule', ['uses' => 'App\Http\Controllers\Admin\GeneralController@appointmentLabSchedule'])->name('admin.appointmentLabSchedule');
 
-Route::get('test', function () {
+Route::get('/', function () {
     return view('welcome');
 });
